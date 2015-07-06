dca9b222c3fe09f94cc056b4df2a54f9821828f1
Ruairidh Carmichael <ruairidhcarmichael@live.co.uk>
2015-07-06 18:06:39 +0100

    Repackage

--- update.sh
+++ update.sh
@@ -1,3 +1,11 @@
+git fetch origin gh-pages
+git merge FETCH_HEAD
+
 ./remove.sh
 
+dpkg-scanpackages -m . /dev/null > Packages
 bzip2 -k Packages
+
+git add --all
+git commit -m "$1"
+git push origin gh-pages
